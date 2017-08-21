dc605a08ec24755e12768319b660f2498b650e39
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-08-21 00:32:11 -0700

    added file

--- DNAPairing.js
+++ DNAPairing.js
@@ -1,5 +1,17 @@
 function pairElement (str) {
-  return str
+  var output = []
+  var pairs = {
+    G: 'C',
+    C: 'G',
+    A: 'T',
+    T: 'A'
+  }
+  for (var i = 0, len = str.length; i < len; i++) {
+    var arr = []
+    arr.push(str[i], pairs[str[i]])
+    output.push(arr)
+  }
+  return output
 }
 
-pairElement('GCG')
+console.log(pairElement('GCG'))
